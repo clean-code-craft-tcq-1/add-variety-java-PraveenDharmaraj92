942c0de8bcb588223214b7a539ce0913cabc4933
PraveenDharmaraj92 <62659035+PraveenDharmaraj92@users.noreply.github.com>
2021-04-01 19:55:32 +0530

    reduced complexity

--- src/main/java/alert/EmailAlerter.java
+++ src/main/java/alert/EmailAlerter.java
@@ -1,32 +1,21 @@
 package alert;
 
 import batteryhealth.BreachType;
+import factory.MailSenderFactory;
+import mail.IMailSender;
 
 /**
  * Concrete class for email alert
  *
  */
 public class EmailAlerter implements IAlerter{
+	
+	String recepient = "a.b@c.com";
 
 	@Override
 	public void alert(BreachType breachType) {
-		sendToEmail(breachType);
+		IMailSender mailTemplate = MailSenderFactory.getInstance().getMailSender(breachType);
+        mailTemplate.sendMail(recepient);
 	}
 	
-    private void sendToEmail(BreachType breachType) {
-        String recepient = "a.b@c.com";
-        switch(breachType) {
-          case TOO_LOW:
-            System.out.printf("To: %s\n", recepient);
-            System.out.println("Hi, the temperature is too low\n");
-            break;
-          case TOO_HIGH:
-            System.out.printf("To: %s\n", recepient);
-            System.out.println("Hi, the temperature is too high\n");
-            break;
-          case NORMAL:
-            break;
-        }
-      }
-
 }
